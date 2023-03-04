3bffcd92c685a373bc169acae839a22290fc7d64
f1v3-dev <f1v3_dev@naver.com>
2023-03-04 18:50:20 +0900

    [싱글톤 컨테이너]

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -5,8 +5,22 @@ import hello.core.member.MemberService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+
+/**
+ * [싱글톤 패턴 문제점]
+ * 싱글톤 패턴을 구현하는 코드 자체가 많이 들어간다.
+ * 의존관계상 클라이언트가 구체 클래스에 의존한다. DIP를 위반한다.
+ * 클라이언트가 구체 클래스에 의존해서 OCP 원칙을 위반할 가능성이 높다.
+ * 테스트하기 어렵다.
+ * 내부 속성을 변경하거나 초기화 하기 어렵다.
+ * private 생성자로 자식 클래스를 만들기 어렵다.
+ * 결론적으로 유연성이 떨어진다.
+ * 안티패턴으로 불리기도 한다.
+ */
 
 public class SingletonTest {
 
@@ -44,15 +58,28 @@ public class SingletonTest {
 
     }
 
-    /**
-     * [싱글톤 패턴 문제점]
-     * 싱글톤 패턴을 구현하는 코드 자체가 많이 들어간다.
-     * 의존관계상 클라이언트가 구체 클래스에 의존한다. DIP를 위반한다.
-     * 클라이언트가 구체 클래스에 의존해서 OCP 원칙을 위반할 가능성이 높다.
-     * 테스트하기 어렵다.
-     * 내부 속성을 변경하거나 초기화 하기 어렵다.
-     * private 생성자로 자식 클래스를 만들기 어렵다.
-     * 결론적으로 유연성이 떨어진다.
-     * 안티패턴으로 불리기도 한다.
-     */
+
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer(){
+
+//        AppConfig appConfig = new AppConfig();
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberService memberService1 = ac.getBean("memberService", MemberService.class);
+        MemberService memberService2 = ac.getBean("memberService", MemberService.class);
+
+        // 참조값이 같은 것을 확인
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+
+        // memberService1 == memberService2
+        assertThat(memberService1).isSameAs(memberService2);
+
+    }
+
+
+
 }
