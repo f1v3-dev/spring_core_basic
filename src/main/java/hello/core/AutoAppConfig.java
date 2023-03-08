cb737fff25c9989106f3bcbc49b09eb16694b61c
f1v3-dev <f1v3_dev@naver.com>
2023-03-08 14:58:29 +0900

    [중복 등록과 충돌]

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,4 +1,6 @@
 package hello.core;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,5 +14,11 @@ import static org.springframework.context.annotation.ComponentScan.*;
         excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
 )
 public class AutoAppConfig {
+
+    // 수동 빈 등록이 우선권 (수동 빈이 자동 빈을 오버라이딩)
+    @Bean(name = "memoryMemberRepository")
+    public MemoryMemberRepository memberRepository(){
+        return new MemoryMemberRepository();
+    }
 
 }
