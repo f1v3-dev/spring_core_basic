82a8b3f2fdbbb6b8a84130f236ed0fd008ab8af8
f1v3-dev <f1v3_dev@naver.com>
2023-03-08 14:25:48 +0900

    [컴포넌트 스캔의 탐색 위치, 기본 스캔 대상]

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -5,7 +5,12 @@ import org.springframework.context.annotation.FilterType;
 import static org.springframework.context.annotation.ComponentScan.*;
 @Configuration
 @ComponentScan(
-        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class))
+//        지정하지않으면 @ComponentScan이 붙은 설정 정보 클래스의 패키지가 시작 위치
+//        (AutoAppConfig -> hello.core 패키지) 따라서 프로젝트 시작 루트 위치에 두자!
+//        basePackages = "hello.core.member",
+//        basePackageClasses = AutoAppConfig.class,
+        excludeFilters = @Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+)
 public class AutoAppConfig {
 
 }
