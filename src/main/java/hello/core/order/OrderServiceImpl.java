8aac71ed7225708b09a061664de60bd2457ae678
f1v3-dev <f1v3_dev@naver.com>
2023-03-11 14:16:00 +0900

    [조회 빈이 2개 이상인 경우 - NoUniqueBeanDefinitionException]

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -8,22 +8,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     // 필드 주입 -> @Autowired 붙여주기
     // DI 프레임워크가 없으면 아무것도 할 수 없음 -> 사용하지말자.
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
-/*
-@RequiredArgsConstructor에서 final 생성자 생성
+
+//    @RequiredArgsConstructor에서 final 생성자 생성
 //    생성자 주입 (불변, 필수 의존관계에서 사용)
 //     생성자가 1개인 경우 @Autowired 생략 가능
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
-*/
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
