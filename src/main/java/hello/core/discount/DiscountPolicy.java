464150dfdb95f03cbb566b8da80fff822b476b86
f1v3-dev <f1v3_dev@naver.com>
2023-03-13 22:07:44 +0900

    [자동, 수동의 올바른 실무 운영 기준]

--- src/main/java/hello/core/discount/DiscountPolicy.java
+++ src/main/java/hello/core/discount/DiscountPolicy.java
@@ -8,4 +8,25 @@ public interface DiscountPolicy {
      * @return 할인 대상 금액
      */
     int discount(Member member, int price);
+/*
+다음과 같이 수동으로 등록할 수도 있다.
+-> 애플리케이션에 광범위하게 영향을 미치는 기술 지원 객체는 수동 빈으로 등록해서 딱! 설정 정보에 바로
+   나타나게 하는 것이 유지보수 하기 좋다.
+
+
+@Configuration
+public class DiscountPolicyConfig {
+    @Bean
+    public DiscountPolicy rateDiscountPolicy() {
+        return new RateDiscountPolicy();
+    }
+
+    @Bean
+    public DiscountPolicy fixDiscountPolicy() {
+        return new FixDiscountPolicy();
+    }
+}
+
+*/
+
 }
