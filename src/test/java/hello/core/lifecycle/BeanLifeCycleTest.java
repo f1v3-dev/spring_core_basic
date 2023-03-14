8e72cfda93007ec19f81cc7e9e523e113b4323c7
f1v3-dev <f1v3_dev@naver.com>
2023-03-14 14:13:12 +0900

    [애노테이션 @PostConstruct, @PreDestroy]

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -25,7 +25,8 @@ public class BeanLifeCycleTest {
 
         // 객체 생성과 초기화를 분리하는 이유는?
         // SRP(단일 책임 원칙)과 같이 역할을 구분하기 하자! -> 유지보수 관점에서 유리
-        @Bean(initMethod = "init", destroyMethod = "close")
+//        @Bean(initMethod = "init", destroyMethod = "close")
+        @Bean
         public NetworkClient networkCilent(){
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("https://hello-spring.dev");
