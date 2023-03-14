5ad504a69bcc30a5165e251dd46e289dd34c710b
f1v3-dev <f1v3_dev@naver.com>
2023-03-14 14:03:35 +0900

    [인터페이스 InitializingBean, DisposableBean]

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,13 +1,14 @@
 package hello.core.lifecycle;
 
-public class NetworkClient {
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+public class NetworkClient implements InitializingBean, DisposableBean {
 
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
     }
 
     public void setUrl(String url) {
@@ -27,4 +28,25 @@ public class NetworkClient {
     public void disconnect(){
         System.out.println("close: " + url);
     }
+    
+    // 초기화 소멸 인터페이스
+    // 단점 : '스프링 전용 인터페이스', 메서드명 변경 불가, 외부 라이브러리에 적용 불가
+    // => 더 좋은 방법으로 사용하지 않음.
+
+    // 의존관계 주입이 끝나면 호출
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    // 빈이 종료될 때 호출
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }
+
+
 }
